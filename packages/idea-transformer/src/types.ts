7f563c41ea99abafebd977d2c05272d03453b732
Rheilly Aguilar <rheilly17@gmail.com>
2024-12-03 19:24:48 +0800

    remove the 3rd and 4th argument and move it in TransformerOption

--- packages/idea-transformer/src/types.ts
+++ packages/idea-transformer/src/types.ts
@@ -1,6 +1,7 @@
 import type FileSystem from '@stackpress/types/dist/filesystem/FileSystem';
 import type { PluginConfig, SchemaConfig } from '@stackpress/idea-parser';
 import type Terminal from './Terminal';
+import FileLoader from '@stackpress/types/dist/filesystem/FileLoader';
 
 //--------------------------------------------------------------------//
 // Terminal Types
@@ -12,7 +13,11 @@ export type TerminalOptions = { cwd?: string, fs?: FileSystem };
 //--------------------------------------------------------------------//
 // Transformer Types
 
-export type TransformerOptions = { cwd?: string, fs?: FileSystem };
+export type TransformerOptions = { 
+  cwd?: string, 
+  fs?: FileSystem 
+  loader?: FileLoader
+};
 
 
 //--------------------------------------------------------------------//
